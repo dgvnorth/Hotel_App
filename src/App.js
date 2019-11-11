0af4fa918e810b5874b72778dff18a4e944bc6a0
dgvnorth <campello03@hotmail.com>
2019-11-11 19:31:03 +0000

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Switch, Route, Link } from "react-router-dom";
-
 import './App.css';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms" component={Rooms} />
