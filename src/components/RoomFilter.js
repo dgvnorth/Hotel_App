967dcc0f486e707b62a424291278f3ad043eabdf
dgvnorth <campello03@hotmail.com>
2019-11-19 19:54:21 +0000

    room type and guests filters added

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -3,7 +3,12 @@ import { useContext } from 'react';
 import { RoomContext } from '../Context';
 import Title from './Title';
 
-export default function RoomsFilter() {
+//get all unique values
+const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))]
+}
+
+export default function RoomsFilter({ rooms }) {
     const context = useContext(RoomContext);
     const {
         handleChange,
@@ -17,12 +22,35 @@ export default function RoomsFilter() {
         pets,
         price
     } = context;
+    // get unique types
+    let types = getUnique(rooms, 'type');
+    //add all
+    types = ['all', ...types]
+    //map to jsx
+    types = types.map((item, index) => {
+        return <option value={item} key={index}>{item}</option>
+    })
 
+    let people = getUnique(rooms, 'capacity');
+    people = people.map((item, index) => {
+        return <option key={index} value={item}>{item}</option>
+    })
     return (
         <section className='filter-container'>
             <Title title='search rooms' />
             <form className='filter-form'>
-
+                {/*select type */}
+                <div className='form-group'>
+                    <label htmlFor='type'>room type</label>
+                    <select name='type' id='type' value={type} className='form-control' onChange={handleChange}>{types}</select>
+                </div>
+                {/*end select type */}
+                {/*guests */}
+                <div className='form-group'>
+                    <label htmlFor='capacity'>guests</label>
+                    <select name='capacity' id='capacity' value={capacity} className='form-control' onChange={handleChange}>{people}</select>
+                </div>
+                {/*end guests */}
             </form>
         </section>
     )
